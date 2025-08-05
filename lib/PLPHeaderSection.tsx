a85633f583d01510f5ba089a5d7768e1ea478647
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-08-05 15:16:50 +0100

    Add section props

--- lib/PLPHeaderSection.tsx
+++ lib/PLPHeaderSection.tsx
@@ -1,12 +1,16 @@
 import DisclosureParagraph from "./DisclosureParagraph";
 import DisclosureParagraphWrapper from "./DisclosureParagraphWrapper";
+import { classMerge } from "./utils/twUtils";
 
 export default function PLPHeaderSection({
   className,
   ...props
 }: React.ComponentProps<"section">) {
   return (
-    <section>
+    <section
+      {...props}
+      className={classMerge("bg-white w-full h-auto", className)}
+    >
       <DisclosureParagraphWrapper>
         <DisclosureParagraph />
       </DisclosureParagraphWrapper>
