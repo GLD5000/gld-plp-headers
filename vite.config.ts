b737df9b3d2371477662249edaa4fa6d5aeaafa3
GLD5000 <garethlouisdevlin@gmail.com>
2025-08-08 19:54:24 +0100

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import tailwindcss from '@tailwindcss/vite'
-import {peerDependencies} from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,7 +14,7 @@ export default defineConfig({
     },
     copyPublicDir: false,
     rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime', ...Object.keys(peerDependencies)],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
