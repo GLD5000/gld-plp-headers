084b1ade9d270c585d57dfe7c16feb43c569e4fc
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-08-08 15:28:24 +0100

    Update Package

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,15 @@
+import BannerTitleH1 from "./components/bannerTitle/bannerTitleH1";
+import ButtonWrapper from "./components/buttons/ButtonWrapper";
+import LinkButton from "./components/buttons/LinkButton";
+import CategoryBannerSection from "./components/CategoryBannerSection";
+import DisclosureParagraph from "./components/disclosureParagraph/DisclosureParagraph";
 import PlpHeaderComplete from "./PlpHeaderComplete";
 
-
-export {PlpHeaderComplete}
+export {
+  PlpHeaderComplete,
+  CategoryBannerSection,
+  BannerTitleH1,
+  DisclosureParagraph,
+  ButtonWrapper,
+  LinkButton,
+};
