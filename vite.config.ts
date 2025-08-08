57703733a078e9a4a443f68a216ea76534b440da
GLD5000 <garethlouisdevlin@gmail.com>
2025-08-08 18:36:02 +0100

    Update package files

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-import tailwindcss from '@tailwindcss/vite'
-import {peerDependencies} from './package.json'
+import tailwindcss from "@tailwindcss/vite";
+import { peerDependencies } from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(), dts({ include: ["lib"] ,tsconfigPath: './tsconfig.lib.json'})],
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({ include: ["lib"], tsconfigPath: "./tsconfig.lib.json" }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
@@ -18,8 +22,8 @@ export default defineConfig({
       external: Object.keys(peerDependencies),
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
